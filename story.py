4e57e35b46dad9a0ef875b9c3cf8a8bb7a58e19c
Siddhant Kumar <69733347+Siddhant099@users.noreply.github.com>
2021-08-20 12:44:59 +0530

    Add files via upload

---
+++ story.py
@@ -0,0 +1,8 @@
+import random
+from time import sleep
+
+mainchar = input('Main Character:')
+rivalname = input("Rival's Name:")
+activity = input("Daily Activities:")
+posadj = input("Postive points")
+negadj = input("Negative Points:")
